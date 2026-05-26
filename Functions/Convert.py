2682d990e6e0deae916ad507dfd075d537e68443
RaeSterize <zeekroczthesecond@gmail.com>
2026-05-26 01:09:34 -0500

    Converting Optiimzation | Pre-Copy Detection Rework

--- Functions/Convert.py
+++ Functions/Convert.py
@@ -4,7 +4,6 @@ from tkinter import messagebox
 import subprocess
 import os
 import threading
-import time
 
 # Variables
 remaining = 0
@@ -13,7 +12,7 @@ videos = 0
 # Main Window
 app = None 
 
-startArgs = None
+startArgs = "ffmpeg"
 
 videoBR = None
 audioBR = None
@@ -28,10 +27,13 @@ outputFile = None
 
 progressWindow = None
 progress = None
-currentVideo = None
+ProcessingText = None
+QueueVideoTitle = "..."
 remaining = None
 
 def ConvertVideos(root, videoCollection, videoNames, output, videoBitRateInt, audioBitRateInt, framesInt):
+
+    print("Button Clicked!!\n Getting Variables...")
 
     global remaining
     global videos
@@ -49,46 +51,60 @@ def ConvertVideos(root, videoCollection, videoNames, output, videoBitRateInt, au
 
     global progressWindow
     global progress
-    global currentVideo
+    global ProcessingText
+    global QueueVideoTitle
     global remaining
 
+    app = root
 
-
+    # Checks to see if the following following values are valid:
+    # - Bitrates & FPS
+    # - Input Videos & Paths 
+    # - Output Videos & Paths 
     try:
 
         videoBR = int(videoBitRateInt)
         audioBR = int(audioBitRateInt)
         fps = int(framesInt)
+        print("Bitrate & FPS set!")
 
+        vidsWithPaths = sorted(videoCollection)
+        vidFiles = sorted(videoNames)
+        print("Input Videos & Paths Set!")
 
+        outputPath = output
+        outputFile = sorted(os.listdir(outputPath))
+        print("Output Videos & Paths Set!")
+        
+    except TypeError:
+        ErrorHandler("No Videos Added")
+        return
+    except FileNotFoundError:
+        ErrorHandler("Output Path Has Not Been Set or Does not Exist")
+        return
+        #messagebox.showwarning(title="Error", message=f"Output Path Has Not Been Set or Does not Exist")
     except ValueError:
-        messagebox.showwarning(title="Option Values Not a Number", message="Please Only Use Numbers for Options")
+        #messagebox.showwarning(title="Option Values Not a Number", message="Please Only Use Numbers for Options")
+        ErrorHandler("Please Only Use Numbers for Options")
         return
     except Exception as e:
-        #messagebox.showerror(title="Unknown Error", message="Unknown Error Has Occured")
-        messagebox.showerror(title="Error", message=f"ERROR! Please Report the following to the Developer!\n\n:{type(e).__name__}")
-        print(e)
+        ErrorHandler(e)
         return
     
-    app = root
-
-    vidsWithPaths = sorted(videoCollection)
-    vidFiles = sorted(videoNames)
-
-    outputPath = output
-    outputFile = sorted(os.listdir(outputPath))
-
-    fileCopies = 0
+    
     videos = 0
+
+    print("Checking Total Videos...")
 
     for i in range(len(vidFiles)):
         videos += 1    
     
-    copiesLoop = True
-    
+    fileCopies = 0
     inputLoop = 0
     outputLoop = 0
 
+
+    # Checking for Video Copies
     try:
 
         global loopInputPath
@@ -97,6 +113,7 @@ def ConvertVideos(root, videoCollection, videoNames, output, videoBitRateInt, au
         loopInputPath = vidsWithPaths.copy()
         loopVids = vidFiles.copy()
         loopOutputVids = outputFile.copy()
+        loopNonCopyRemainder = videos
 
         # While logged videos from Output is not empty, run this code
         while loopOutputVids: 
@@ -132,7 +149,7 @@ def ConvertVideos(root, videoCollection, videoNames, output, videoBitRateInt, au
                     print(f"Resetting Loops...\nInput: {outputLoop}\nOutput: {inputLoop}\nRemoving video from total videos...\n")
                     outputLoop = 0
                     inputLoop = 0
-                    videos -= 1
+                    loopNonCopyRemainder -= 1
             else:
                 print("Not a copy, skipping...")
                 print("Increasing Input Loop...\n")
@@ -146,79 +163,69 @@ def ConvertVideos(root, videoCollection, videoNames, output, videoBitRateInt, au
                     print("Resetting output Input...")
                     outputLoop = 0
 
-
         if fileCopies > 0:
             print(f"{fileCopies} Copies Found!!")
         else:
             print("No Copies found!!")
 
-            
-                    
     except Exception as e:
-        messagebox.showerror(title="Error", message=f"ERROR! Please Report the following to the Developer!\n\nDectecting Copies Failed\n:{type(e).__name__}: {e}")
-
-    
+        ErrorHandler(e)
 
     print(f"{vidFiles}\n{vidsWithPaths}")
 
-
-    print(videos)
+    print(f"Videos in Input Folder: {videos}")
+    print(f"Copied Files found: {loopNonCopyRemainder}")
 
     print(f"Videos from Loop: {loopVids}")
 
-    # Checks if Output is Valid
-    if os.path.exists(outputPath):
 
-        startConvert = messagebox.askquestion(title="Confirm?", message="Do You Want to Convert Videos?")
-        
-        if startConvert == "yes":
+    startConvert = messagebox.askquestion(title="Confirm?", message="Do You Want to Convert Videos?")
+    
+    if startConvert == "yes":
 
-            startArgs = "ffmpeg"
+        #startArgs = "ffmpeg"
 
-            if fileCopies > 0:
-                
-                overwriteAnswer = messagebox.askyesnocancel(icon="warning",title="Overwrite?", message=f"There are {fileCopies} videos that share the same name.\nDo you wish to overwrite them?")
-                
-                if overwriteAnswer == True:
-                    startArgs = "ffmpeg -y"
-                elif overwriteAnswer == False:
-                    vidFiles = loopVids
-                    vidsWithPaths = loopInputPath
+        if fileCopies > 0:
 
-            if videos <= 0:
-                return
+            warningText = f"There are {fileCopies} videos that share the same name."
+            if loopNonCopyRemainder == 0:
+                warningText = "All selected files share the same name."
 
-            print(startArgs)
-        
-            progressWindow = Toplevel()
-
-            progressWindow.update()
-            progressWindow.geometry("350x90")
-
-            progress = ttk.Progressbar(progressWindow, orient=HORIZONTAL, length=300, mode="determinate")
-            progress.place(x=20, y=50)
-
-
-            currentVideo = Label(progressWindow,text="Processing: ")
-            currentVideo.place(x=20, y=18)
+            overwriteAnswer = messagebox.askyesnocancel(icon="warning",title="Overwrite?", message=f"{warningText}\nDo you wish to overwrite them?")
             
-            remaining = 100 / videos
-            print(f"Remaining: {remaining}")
-
-            vidioBRStr = str(videoBR)
-            audioBRStr = str(audioBR)
-            fpsStr = str(fps)
-
-            options = f'-b:v {vidioBRStr}k -b:a {audioBRStr}k -r {fpsStr}'
-
-            print("Starting Thread...")
-            StartFFmpeg() 
-            
-            return remaining, videos, app, startArgs, videoBR, audioBR, fps, vidsWithPaths, vidFiles, options, outputPath, outputFile, progressWindow, progress, currentVideo, remaining
+            if overwriteAnswer == True:
+                startArgs = "ffmpeg -y"
+            elif overwriteAnswer == False:
+                videos = loopNonCopyRemainder
+                vidFiles = loopVids
+                vidsWithPaths = loopInputPath
+                if videos == 0:
+                    return
 
 
-    else:
-        messagebox.showerror(title="Folders Not Set",message="Folders Not Found, Please Setup Folder Paths.")    
+    
+        # Progress Bar Window Creation
+        progressWindow = Toplevel()
+        #progressWindow.update()
+        progressWindow.geometry("350x90")
+        progress = ttk.Progressbar(progressWindow, orient=HORIZONTAL, length=300, mode="determinate")
+        progress.place(x=20, y=50)
+        ProcessingText = Label(progressWindow,text=f"Converting: {QueueVideoTitle}")
+        ProcessingText.place(x=20, y=18)
+        
+        remaining = 100 / videos
+        print(f"Remaining: {remaining}")
+
+        vidioBRStr = str(videoBR)
+        audioBRStr = str(audioBR)
+        fpsStr = str(fps)
+
+        options = f'-b:v {vidioBRStr}k -b:a {audioBRStr}k -r {fpsStr}'
+
+        print("Starting Thread...")
+        StartFFmpeg() 
+        
+        return remaining, videos, app, startArgs, videoBR, audioBR, fps, vidsWithPaths, vidFiles, options, outputPath, outputFile, progressWindow, progress, ProcessingText, QueueVideoTitle, remaining
 
 # ===========
 #   Threads
@@ -230,8 +237,16 @@ def StartFFmpeg():
 
 def FFmpegThread():
 
+    QueueTitleLimit = 40
+
     try:
         print("Thread Entered!!")
+        
+        QueueVideoTitle = vidFiles[0]
+        if len(QueueVideoTitle) > QueueTitleLimit:
+            QueueVideoTitle = f"{QueueVideoTitle[0:QueueTitleLimit]}..."
+
+        ProcessingText.config(text=f"Converting: {QueueVideoTitle}")
 
         outputVar = f"{outputPath}/{vidFiles[0]}"
 
@@ -241,7 +256,7 @@ def FFmpegThread():
         subprocess.run(ffmpeg_cmd, check=True)
         app.after(20, VideoQueue())
     except Exception as e:
-        messagebox.showerror(title="Error", message=f"ERROR! Please Report the following to the Developer!\n\nDectecting Copies Failed\n:{type(e).__name__}: {e}")
+        ErrorHandler(e)
         print(f"{type(e).__name__}: {e}")
 
 
@@ -249,16 +264,24 @@ def VideoQueue():
 
     print("Entered Queue!!")
 
+    print("Adding to Progress Bar...")
     progress['value'] += remaining
 
-    print(progress['value'])
+    print(f"Video Progress: {int(progress['value'])}%")
 
     vidsWithPaths.pop(0)
     vidFiles.pop(0)
 
-    if vidsWithPaths and  videos:
+    if vidsWithPaths:# and  videos:
         print("Loading Next Video...")
         FFmpegThread()
     else:
+        ProcessingText.config(text=f"Converting Complete!")
         messagebox.showinfo(title="Conversion Successful!", message="Files Have Been Converted!")  
         progressWindow.destroy()  
+
+def ErrorHandler(e):
+    if e == "e":
+        messagebox.showerror(title="Error", message=f"ERROR! Please Report the following to the Developer!\n{type(e).__name__}: {e}")
+    else:
+        messagebox.showwarning(title="Error", message=f"{e}")
